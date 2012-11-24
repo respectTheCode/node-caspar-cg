b2c69f5c177c7c20e28173382ec8af69e6089d0c
Kevin Smith <respectTheCode@me.com>
2012-11-24 11:35:43 -0500

    finish template commands

--- test.js
+++ test.js
@@ -21,6 +21,18 @@ ccg.connect(function () {
 		ccg.loadData("SomeData", function (err, data) {
 			console.log("data:", data);
 		});
+
+		ccg.loadTemplate("1-2", "Gymnastics/LT-SINGLE NAME", "SomeData", function () {
+			ccg.playTemplate("1-2");
+
+			setTimeout(function () {
+				ccg.updateTemplateData("1-2", {f0: "Someones Name", f1: "Some title"});
+			}, 2 * 1000);
+
+			setTimeout(function () {
+				ccg.stopTemplate("1-2");
+			}, 4 * 1000);
+		});
 	});
 
 	// ccg.getMediaFiles(function (err, serverInfo) {
@@ -48,10 +60,8 @@ ccg.connect(function () {
 	// });
 
 	setTimeout(function () {
-		ccg.clear("1");
-	}, 1 * 1000);
-
-	setTimeout(function () {
-		ccg.disconnect();
+		ccg.clear("1", function () {
+			ccg.disconnect();
+		});
 	}, 5 * 1000);
 });
