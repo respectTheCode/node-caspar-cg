f6e4fdcf8d604eac241231930869cb0cb4b53491
Kevin Smith <kevin@app-press.com>
2013-11-22 07:21:03 -0500

    better test of failing with multiple instances

--- test/connection.js
+++ test/connection.js
@@ -6,93 +6,146 @@ var assert = require("chai").assert;
 var CasparCG = require("../");
 
 var debug = false;
+var port = 8000;
 
 describe("connection", function () {
-	var ccg;
-	var connection;
+	var ccg1;
+	var server1;
+	var connection1;
 
-	before(function (done) {
-		var server = new net.createServer();
-		server.listen(1234);
+	beforeEach(function (done) {
+		if (server1) server1.close();
+		server1 = new net.createServer();
+		server1.listen(port);
 
-		server.on("connection", function (socket) {
-			connection = socket;
+		server1.on("connection", function (socket) {
+			connection1 = socket;
 			done();
 		});
 
-		ccg = new CasparCG({
+		ccg1 = new CasparCG({
 			host: "localhost",
-			port: 1234,
+			port: port,
 			debug: debug
 		});
 
-		ccg.connect();
+		ccg1.connect();
+		port++;
 	});
 
-	it("should connect", function (done) {
-		connection.on("data", function (data) {
-			data = data.toString();
-
-			assert.equal(data, "PLAY 1-1 \"AMB\"\r\n", "data is a play command");
-
-			done();
+	it("should connect and send 1 command", function (done) {
+		connection1.on("data", function (data) {
+			assert.equal(data.toString(), "PLAY 1-0 \"AMB\"\r\n", "data is a play command");
+			this.write("202 Play OK\r\n");
 		});
 
-		ccg.play("1-1", "AMB");
+		ccg1.play("1-0", "AMB", done);
 	});
 
 	describe("with 2 connections", function () {
 		var ccg2;
+		var server2;
 		var connection2;
+		var ccg3;
+		var server3;
+		var connection3;
 
-		before(function (done) {
-			var server = new net.createServer();
-			server.listen(1235);
+		beforeEach(function (done) {
+			if (server2) server2.close();
+			if (server3) server3.close();
 
-			server.on("connection", function (socket) {
+			server2 = new net.createServer();
+			server2.listen(port);
+
+			server2.on("connection", function (socket) {
 				connection2 = socket;
-				done();
+				
+				server3 = new net.createServer();
+				server3.listen(port);
+
+				server3.on("connection", function (socket) {
+					connection3 = socket;
+					done();
+				});
+
+				ccg3 = new CasparCG({
+					host: "localhost",
+					port: port,
+					debug: debug
+				});
+
+				ccg3.connect();
+				port++;
 			});
 
 			ccg2 = new CasparCG({
 				host: "localhost",
-				port: 1235,
+				port: port,
 				debug: debug
 			});
 
 			ccg2.connect();
+			port++;
 		});
 
-		it("should connect on connection 1", function (done) {
-			connection.on("data", function (data) {
-				data = data.toString();
+		var cleanup = function () {
+			connection1.removeAllListeners("data");
+			connection2.removeAllListeners("data");
+			connection3.removeAllListeners("data");
+		};
 
-				assert.equal(data, "PLAY 2-1 \"AMB\"\r\n", "data is a play command");
-
-				done();
+		it("should only send to connection 1", function (done) {
+			connection1.on("data", function (data) {
+				assert.equal(data.toString(), "PLAY 1-1 \"AMB\"\r\n", "data is a play command");
+				this.write("202 Play OK\r\n");
+				cleanup();
 			});
-
-			ccg.play("1-1", "AMB");
-		});
-
-		it("should connect on connection 2", function (done) {
 			connection2.on("data", function (data) {
-				data = data.toString();
-
-				assert.equal(data, "PLAY 2-1 \"AMB\"\r\n", "data is a play command");
-
-				done();
+				assert(false, "should not get data on 1");
+				cleanup();
+			});
+			connection3.on("data", function (data) {
+				assert(false, "should not get data on 2");
+				cleanup();
 			});
 
-			ccg2.play("2-1", "AMB");
+			ccg1.play("1-1", "AMB", done);
 		});
 
-		after(function () {
-			ccg2.close();
-		});
-	});
+		it("should only send to connection 2", function (done) {
+			connection2.on("data", function (data) {
+				assert.equal(data.toString(), "PLAY 2-1 \"AMB\"\r\n", "data is a play command");
+				this.write("202 Play OK\r\n");
+				cleanup();
+			});
+			connection1.on("data", function (data) {
+				assert(false, "should not get data on 0");
+				cleanup();
+			});
+			connection3.on("data", function (data) {
+				assert(false, "should not get data on 2");
+				cleanup();
+			});
 
-	after(function () {
-		ccg.close();
+			ccg2.play("2-1", "AMB", done);
+		});
+
+		it("should only send to connection 3", function (done) {
+			connection3.on("data", function (data) {
+				assert.equal(data.toString(), "PLAY 3-1 \"AMB\"\r\n", "data is a play command");
+				this.write("202 Play OK\r\n");
+				cleanup();
+			});
+			connection1.on("data", function (data) {
+				assert(false, "should not get data on 0");
+				cleanup();
+			});
+			connection2.on("data", function (data) {
+				assert(false, "should not get data on 1");
+				cleanup();
+			});
+
+			ccg3.play("3-1", "AMB", done);
+		});
 	});
 });
